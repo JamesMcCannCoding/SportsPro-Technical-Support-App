a9ecd48f4f85c14715a04b05f8e613ca7576074f
JamesMcCannCoding <144072603+JamesMcCannCoding@users.noreply.github.com>
2023-09-06 14:01:35 +0800

    Update index.php
    Added formatted date settings to allow user to enter in any style of date and it will be converted and accepted by mySQL.

--- product_manager/index.php
+++ product_manager/index.php
@@ -9,7 +9,6 @@ if ($action === NULL) {
         $action = 'list_products';  // Default action when page is loaded.
     }
 }
-
 // List Products:
 if ($action == 'list_products') {
     // Get product data
@@ -20,18 +19,19 @@ if ($action == 'list_products') {
 // Delete product:
 } else if ($action == 'delete_product') {
     $product_code = filter_input(INPUT_POST, 'product_code');
-    // Ensure $product_code is not empty and contains a valid product code
+    // Ensure $product_code is not empty and contains a valid product code.
     if (!empty($product_code)) {
-        // Call the delete_product function
+        // Call the delete_product function.
         if (delete_product($product_code)) {
-            // Deletion was successful, you can redirect back to the product list page
+            // Deletion was successful, you can redirect back to the product list page.
             header("Location: index.php?action=list_products");
     }
 }
-// Shows the product add form
+// Shows the product add form.
 } else if ($action == 'show_add_form') {
     include('product_add.php');
-// Product add form validation
+    
+// Product add form validation:
 } else if ($action == 'add_product') {
     // Get data from form
     $code = filter_input(INPUT_POST, 'code');
@@ -39,52 +39,57 @@ if ($action == 'list_products') {
     $version = filter_input(INPUT_POST, 'version', FILTER_VALIDATE_FLOAT);
     $release_date = filter_input(INPUT_POST, 'release_date');
 
-    // Create a new array to store error messages
+    // Create a new array to store error messages:
     $errors = array();
-
-    // Validate product code
+    
+    // Validate product code:
     if (empty($code)) {
         $errors['code'] = 'Product code is required.';
     }
-
-    // Validate product Name
+    // Validate product name:
     if (empty($name)) {
         $errors['name'] = 'Product name is required';
     }
-
-    // Validate version
+    // Validate version:
     if (empty($version)) {
         $errors['version'] = 'Product version is required.';
     }
-
-    // Validate release date
-    if (empty($release_date)) {
+     // Validate release date:
+     if (empty($release_date)) {
         $errors['release_date'] = 'Product release date is required.';
-    }
-
-    // Check if the product code already exists
+    } else {
+        // Attempt to convert the user input into a consistent format (e.g., yyyy-mm-dd) using strtotime php function.
+        $release_date = strtotime($release_date);
+        
+        // Check if the date conversion was successful and the date is not the Unix epoch (1/1/1970) using logical or.
+        if ($release_date === false || date('Y-m-d', $release_date) == '1970-01-01') {
+            // Handle the case where the date input is not valid.
+            $errors['release_date'] = 'The date specified is not valid.';
+        } else {
+            // Convert the valid date to the desired format.
+            $release_date = date('Y-m-d', $release_date);
+        }
+    // Check if the product code already exists:
     $existing_product = get_product($code);
     if ($existing_product) {
         $errors['code'] = 'Product code already exists.';
+        } 
     }
-
-    // If there are validation errors, include the product_error.php file
+    // If there are validation errors, include the product_error.php file.
     if (!empty($errors)) {
         include('product_error.php');
     } else {
-        // All validation is passed, proceed with adding the product to mySQL
+        // All validation is passed, proceed with adding the product to mySQL.
         $addSuccess = add_product($code, $name, $version, $release_date);
 
         if ($addSuccess) {
-            // Redirect to the product list
+            // Redirect to the product list.
             header('Location: ?action=list_products&add_success=true');
         } else {
-            // Handle the case where the adding fails
+            // Handle the case where the adding fails.
             $errorMessage = "Failed to add the product.";
             include('product_error.php');
         }
     }
 }
-
-
 ?>
