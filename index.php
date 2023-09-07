14eab9babb02a9b0d306b41560ad20204d3a1549
JamesMcCannCoding <144072603+JamesMcCannCoding@users.noreply.github.com>
2023-09-07 16:46:46 +0800

    Update index.php
    7/9/23 - Update on index.php home page to include new display incidents and new assign incidents pages.

--- index.php
+++ index.php
@@ -1,21 +1,22 @@
 <?php include 'view/header.php'; ?>
 <main>
-    <nav>    
+    <nav>
+        <br>    
     <h2>Administrators</h2>
     <ul>
         <li><a href="product_manager">Manage Products</a></li>
         <li><a href="technician_manager">Manage Technicians</a></li>
         <li><a href="customer_manager">Manage Customers</a></li>
         <li><a href="create_incident">Create Incident</a></li>
-        <li><a href="under_construction.php">Assign Incident</a></li>
-        <li><a href="under_construction.php">Display Incidents</a></li>
+        <li><a href="assign_incident">Assign Incident</a></li>
+        <li><a href="display_incidents">Display Incidents</a></li>
     </ul>
-
+        <br>
     <h2>Technicians</h2>
     <ul>
         <li><a href="under_construction.php">Update Incident</a></li>
     </ul>
-
+        <br>
     <h2>Customers</h2>
     <ul>
         <li><a href="product_register">Register Product</a></li>
