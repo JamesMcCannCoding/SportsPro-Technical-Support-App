ac1fd125a95be19d6f75c35ffc204b72cf4c4c0e
JamesMcCannCoding <144072603+JamesMcCannCoding@users.noreply.github.com>
2023-09-08 21:37:02 +0800

    Update customer_login.php

--- product_register/customer_login.php
+++ product_register/customer_login.php
@@ -1,4 +1,6 @@
-<?php include '../view/header.php'; ?>
+<?php 
+session_start();
+include '../view/header.php'; ?>
 <main>
     <h1>Customer Login</h1>
     <p>You must login before you can register a product.</p>
@@ -7,10 +9,12 @@
         <input type="hidden" name="action" value="login_customer">
         <label for="email">Email:</label>
         <input type="text" id="email" name="email" required>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required>
         <input type="submit" value="Login">
     </form>
     <?php
-    // Display error message if email does not exist.
+    // Display error message if email or password is incorrect.
     if (isset($error_message)) {
         echo '<p>' . $error_message . '</p>';
     }
