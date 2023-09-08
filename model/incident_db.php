4b33306e4bc7c5eb05c35356d964f71d0abd0c16
JamesMcCannCoding <144072603+JamesMcCannCoding@users.noreply.github.com>
2023-09-08 21:39:20 +0800

    Update incident_db.php

--- model/incident_db.php
+++ model/incident_db.php
@@ -1,31 +1,72 @@
 <?php
 
+function get_incidents_assign() {
+    global $db;
+    $query = 'SELECT incidents.*, CONCAT(customers.firstName, " ", customers.lastName) AS customer_name, CONCAT(technicians.firstName, " ", technicians.lastName) AS technician_name
+              FROM incidents
+              INNER JOIN customers ON incidents.customerID = customers.customerID
+              LEFT JOIN technicians ON incidents.techID = technicians.techID
+              WHERE (incidents.dateClosed IS NULL OR incidents.dateClosed = "") AND incidents.techID IS NULL
+              ORDER BY incidents.dateOpened ASC'; 
+    $statement = $db->prepare($query);
+    $statement->execute();
+    $incidents = $statement->fetchAll();
+    $statement->closeCursor();
+    return $incidents;
+}
+
+//Get incidents function.
+function get_incidents() {
+    global $db;
+    $query = 'SELECT incidents.*, CONCAT(customers.firstName, " ", customers.lastName) AS customer_name, CONCAT(technicians.firstName, " ", technicians.lastName) AS technician_name
+              FROM incidents
+              INNER JOIN customers ON incidents.customerID = customers.customerID
+              LEFT JOIN technicians ON incidents.techID = technicians.techID
+              WHERE incidents.dateClosed IS NULL OR incidents.dateClosed = ""
+              ORDER BY incidents.dateOpened ASC';
+    $statement = $db->prepare($query);
+    $statement->execute();
+    $incidents = $statement->fetchAll();
+    $statement->closeCursor();
+    return $incidents;
+}
+
+// Function to complete an incident by setting the dateClosed.
+function complete_incident($incidentID) {
+    global $db;
+    // Set the current date and time as the dateClosed.
+    $dateClosed = date('Y-m-d H:i:s');
+    // Define the SQL query to update the incident:
+    $query = 'UPDATE incidents
+              SET dateClosed = :dateClosed
+              WHERE incidentID = :incidentID';
+    // Prepare and execute the query:
+    $statement = $db->prepare($query);
+    $statement->bindValue(':dateClosed', $dateClosed);
+    $statement->bindValue(':incidentID', $incidentID);
+    $statement->execute();
+    $statement->closeCursor();
+}
+
 // Function to insert a new incident into the database
 function add_incident($firstName, $lastName, $productName, $title, $description) {
     global $db;
-
-    // Get customerID based on firstName and lastName (You need to implement this function)
+    // Get customerID based on firstName and lastName.
     $customerID = get_customer_id_by_name($firstName, $lastName);
-
-    // Get productCode based on productName (You need to implement this function)
+    // Get productCode based on productName.
     $productCode = get_product_code_by_name($productName);
-
-    // Set the technician ID (You need to fetch this from session or another source)
+    // Set the technician ID.
     $techID = 1;
-
-    // Set the current date as the opened date
+    // Set the current date as the opened date.
     $dateOpened = date('Y-m-d');
-
-    // Set the dateClosed to null initially
+    // Set the dateClosed to null initially.
     $dateClosed = null;
-
-    // Define the SQL query
+    // Define the SQL query:
     $query = 'INSERT INTO incidents
                  (customerID, productCode, techID, dateOpened, dateClosed, title, description)
               VALUES
                  (:customerID, :productCode, :techID, :dateOpened, :dateClosed, :title, :description)';
-    
-    // Prepare and execute the query
+    // Prepare and execute the query:
     $statement = $db->prepare($query);
     $statement->bindValue(':customerID', $customerID);
     $statement->bindValue(':productCode', $productCode);
@@ -37,5 +78,4 @@ function add_incident($firstName, $lastName, $productName, $title, $description)
     $statement->execute();
     $statement->closeCursor();
 }
-
 ?>
