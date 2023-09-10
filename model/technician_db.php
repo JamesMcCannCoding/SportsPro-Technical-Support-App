827d3ea26cc10096c07b4022253593ac6a48345a
JamesMcCannCoding <144072603+JamesMcCannCoding@users.noreply.github.com>
2023-09-10 16:45:21 +0800

    Update technician_db.php
    Updated get_technicians() function to incorporate new joining of first and last name feature under the name column.

--- model/technician_db.php
+++ model/technician_db.php
@@ -2,7 +2,7 @@
 
 function get_technicians() {
     global $db;
-    $query = 'SELECT * FROM technicians';
+    $query = 'SELECT CONCAT(firstName, " ", lastName) AS name, email, phone, password FROM technicians';
 
     $statement = $db->prepare($query);
     $statement->execute();
@@ -10,6 +10,7 @@ function get_technicians() {
     $statement->closeCursor();
     return $technicians;
 }
+
 
 function delete_technician($technician_id) {
     global $db;
@@ -46,6 +47,39 @@ function add_technician($first_name, $last_name, $email, $phone, $password) {
 }
 
 function update_technician($technician_id, $first_name, $last_name, $email, $phone, $password) {
-    // ??
+    global $db;
+
+    $query = "UPDATE technicians
+              SET firstName = :firstName, lastName = :lastName, email = :email, phone = :phone, password = :password
+              WHERE techID = :technician_id";
+
+    $statement = $db->prepare($query);
+    $statement->bindValue(':technician_id', $technician_id);
+    $statement->bindValue(':firstName', $first_name);
+    $statement->bindValue(':lastName', $last_name);
+    $statement->bindValue(':email', $email);
+    $statement->bindValue(':phone', $phone);
+    $statement->bindValue(':password', $password);
+
+    // Technician boolean result for updating
+    if ($statement->execute()) {
+        return true; // Technician update successful
+    } else {
+        return false; // Technician update failed
+    }
+    $statement->closeCursor();
+}
+
+// Function to assign a technician to an incident
+function assign_technician($incidentID, $techID) {
+    global $db;
+    $query = 'UPDATE incidents
+              SET techID = :techID
+              WHERE incidentID = :incidentID';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':techID', $techID);
+    $statement->bindValue(':incidentID', $incidentID);
+    $statement->execute();
+    $statement->closeCursor();
 }
 ?>
