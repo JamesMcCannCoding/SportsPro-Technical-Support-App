b5c1dcc8e606f1282ebb4f8acdbef5293cf35339
JamesMcCannCoding <144072603+JamesMcCannCoding@users.noreply.github.com>
2023-09-20 15:26:33 +0800

    Update register_success.php

--- product_register/register_success.php
+++ product_register/register_success.php
@@ -1,7 +1,19 @@
+<?php
+if(session_status() !== PHP_SESSION_ACTIVE) {
+    session_start();
+}
+?>
+    
 <?php include '../view/header.php'; ?>
 <main>
     <h1>Success</h1>
     <p>The registration was a success.</p>
+    
+    <nav>
+        <p>
+            <a href="index.php?action=register_product_form">Go back</a>
+        </p>
+    </nav>
    
 </main>
 <?php include '../view/footer.php'; ?>
