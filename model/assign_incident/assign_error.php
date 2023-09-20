b0cef5da2de15027ae7be7fa02560ac0391f51d8
JamesMcCannCoding <144072603+JamesMcCannCoding@users.noreply.github.com>
2023-09-20 15:18:30 +0800

    Delete model/assign_incident directory

--- model/assign_incident/assign_error.php
+++
@@ -1,9 +0,0 @@
-<?php include '../view/header.php'; ?>
-<main>
-    <h1>Error</h1>
-    <div class="error-messages">
-        <p><?php echo isset($error_message) ? $error_message : 'An error occurred.'; ?></p>
-    </div>
-    <p><a href="?action=list_incidents">Go Back</a></p>
-</main>
-<?php include '../view/footer.php'; ?>
