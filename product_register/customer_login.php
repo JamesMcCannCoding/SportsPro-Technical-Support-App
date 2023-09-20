8463bf1449e66882bf0049f90b115b1ac49ffd75
JamesMcCannCoding <144072603+JamesMcCannCoding@users.noreply.github.com>
2023-09-20 15:24:48 +0800

    Update customer_login.php

--- product_register/customer_login.php
+++ product_register/customer_login.php
@@ -1,17 +1,31 @@
 <?php 
-session_start();
-include '../view/header.php'; ?>
+if(session_status() !== PHP_SESSION_ACTIVE) {
+    session_start();
+}
+?>
+
+<?php include '../view/header.php'; ?>
 <main>
+    <!-- Displays the customer log in field. -->
     <h1>Customer Login</h1>
     <p>You must login before you can register a product.</p>
     <!-- Display a login form -->
-    <form action="index.php" method="post">
+    <form action="index.php" method="post" class="vertical-form">
         <input type="hidden" name="action" value="login_customer">
-        <label for="email">Email:</label>
-        <input type="text" id="email" name="email" required>
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required>
-        <input type="submit" value="Login">
+        
+        <div class="form-group">
+            <label for="email">Email:</label>
+            <input type="text" id="email" name="email" required>
+        </div>
+        
+        <div class="form-group">
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
+        </div>
+
+        <div class="form-group">
+            <input type="submit" value="Login">
+        </div>
     </form>
     <?php
     // Display error message if email or password is incorrect.
