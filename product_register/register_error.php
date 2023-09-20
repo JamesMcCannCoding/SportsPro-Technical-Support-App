61bd7a120edf9ad7ab3d232b85b2cef7be9722a6
JamesMcCannCoding <144072603+JamesMcCannCoding@users.noreply.github.com>
2023-09-20 15:26:07 +0800

    Update register_error.php

--- product_register/register_error.php
+++ product_register/register_error.php
@@ -1,7 +1,19 @@
+<?php
+if(session_status() !== PHP_SESSION_ACTIVE) {
+    session_start();
+}
+?>
+
 <?php include '../view/header.php'; ?>
 <main>
     <h1>error</h1>
     <p>The registration failed.</p>
+
+    <nav>
+        <p>
+            <a href="index.php?action=register_product_form" class="btn btn-primary">Go back</a>
+        </p>
+    </nav>
    
 </main>
 <?php include '../view/footer.php'; ?>
