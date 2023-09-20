58c6ff6fe1746145ae8c545781c390adfa547994
JamesMcCannCoding <144072603+JamesMcCannCoding@users.noreply.github.com>
2023-09-20 15:23:43 +0800

    Update product_list.php

--- product_manager/product_list.php
+++ product_manager/product_list.php
@@ -1,7 +1,7 @@
 <?php include '../view/header.php'; ?>
 <main>
     <h1>Product List</h1>
-    <!-- display a table of products -->
+    <!-- Displays a table of products -->
     <table>
         <tr>
             <th>Code</th>
@@ -21,7 +21,7 @@
                 <form action="index.php" method="post">
                     <input type="hidden" name="action" value="delete_product">
                     <input type="hidden" name="product_code" value="<?php echo htmlspecialchars($product['productCode']); ?>">
-                    <input type="submit" value="Delete">
+                    <button type="submit">Delete</button>
                 </form>
             </td>
         </tr>
