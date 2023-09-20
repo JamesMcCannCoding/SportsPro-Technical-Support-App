9bcd888d82980104f84c8063fd8c8d9a244eb08f
JamesMcCannCoding <144072603+JamesMcCannCoding@users.noreply.github.com>
2023-09-20 15:30:00 +0800

    Update header.php

--- view/header.php
+++ view/header.php
@@ -1,16 +1,21 @@
 <!DOCTYPE html>
 <html>
 
-<!-- the head section -->
+<!-- The head section -->
 <head>
     <title>SportsPro Technical Support</title>
     <link rel="stylesheet" type="text/css"
           href="/tech_support/main.css">
+    <link rel="icon" 
+            href="/tech_support/SportsProLogo.png" type="image/x-icon">
 </head>
 
-<!-- the body section -->
+<!-- The body section -->
 <body>
 <header>
+    <a href="/tech_support/index.php">
+        <img src="/tech_support/SportsProLogo.png" alt="Logo" width="150" height="100" title="Home">
+    </a>
     <h1>SportsPro Technical Support</h1>
     <p>Sports management software for the sports enthusiast</p>
     <nav>
