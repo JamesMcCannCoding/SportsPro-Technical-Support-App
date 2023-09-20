7f68a545fc4da12c2e71828d97b034f6aec30e64
JamesMcCannCoding <144072603+JamesMcCannCoding@users.noreply.github.com>
2023-09-20 15:25:50 +0800

    Update product_register.php

--- product_register/product_register.php
+++ product_register/product_register.php
@@ -1,3 +1,9 @@
+<?php
+if(session_status() !== PHP_SESSION_ACTIVE) {
+    session_start();
+}
+?>
+
 <?php include '../view/header.php'; ?>
 <main>
     <h2>Register Product</h2>
@@ -13,7 +19,7 @@
     ?>
 
     <form action="index.php" method="post" id="aligned">
-        <input type="hidden" name="action" value="register_product">
+        <input type="hidden" name="action" value="register_product_submit">
         
         <?php if (isset($customer)) : ?>
             <p>Product: <select name="productCode">
@@ -31,9 +37,15 @@
             <p>Customer information not available. Please log in first.</p>
         <?php endif; ?>
     </form>
+
+    <!-- Display logged in email message -->
+    <?php if (isset($_SESSION['email'])) : ?>
+        <p>You are logged in as <?php echo $_SESSION['email']; ?></p>
+    <?php endif; ?>
+
     <form action="customer_logout.php" method="post">
         <input type="hidden" name="action" value="logout">
-    <input type="submit" value="Logout">
-</form>
+        <input type="submit" value="Logout">
+    </form>
 </main>
 <?php include '../view/footer.php'; ?>
